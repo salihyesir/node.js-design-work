bd06306a0bc3a83820b9e3685b6847fd893fdb94
BthnGlryz <32592807+BthnGlryz@users.noreply.github.com>
2017-10-31 16:15:44 +0300

    Database Update!
    Veritabanı ile ilgili eklemeler yapıldı

--- app_server/models/user.js
+++ app_server/models/user.js
@@ -5,7 +5,10 @@ var Schema = mongoose.Schema;
 var userSchema = new Schema({
     userName:{ type: String, required:true, unique:true},
     mail:String,
-    password:{ type: String, required: true}
+    password: { type: String, required: true},
+    avatar: { type:Buffer, contentType:String,default:null},
+    isDeleted: { type:Boolean, required:true , default:false},
+    friend:[String]
 });
 
 var users=mongoose.model('users',userSchema);
