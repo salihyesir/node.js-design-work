5d72ac600e7b940ee7dcc2deff22c8beafb5e6ce
Salih Yesir <salih.yesir@ogr.sakarya.edu.tr>
2017-11-24 23:24:55 +0300

    Multi konferans özelliği eklendi
    Şu anlık WebRTC'nin EasyRTC framework kullanmaktayız. EasyRTC, node.js üzerine inşa edilmiş javascript sunucudur. WebRTC'nin bize verdiği imkanları ve kolaylıklarını bu framework ile kullanmamız daha kolay hale gelecek.

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
 var http    = require("http");              // http server core module
-var express = require("express");           // web framework external module
+var easyrtc = require("easyrtc");           // easyRTC:  full-stack webrtc'nin üzerine kurulu bir çerçevedir. bize biraz daha kolaylık sağlıycak.
 var io      = require("socket.io");         // web socket external module
-var easyrtc = require("easyrtc");           // EasyRTC external module
-
 var path=require('path');
 
+
+//web framework olan express' i yüklüyoruz ve konfigrasyonunu yapıyoruz.
+var express = require("express");    
 var app = express();
+//public klasörümü uygulamaya static olarak bildiriyoruz.
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
+
+// easyrtc sunucusunu başlattık.
+//https://easyrtc.com/docs/easyrtc_faq.php => bu adresten detaylara bakabilirsiniz.
+//express serverda socket.io başlattık
+var webServer = app.listen(8000);
+//WebRTC'nin çalışması için socket.io çalıştırılır.
+var socketServer = io.listen(webServer);
+// EasyRTC server başlatılır.
+var easyrtcServer = easyrtc.listen(app, socketServer, {'apiEnable':'true'});
+
+//viewengine olarak jade yerine ejs yi tercih ettik.
 var ejsLayouts = require('express-ejs-layouts');
+app.set('view engine','ejs');
+app.use(ejsLayouts);
 
+
+//form verilerinin ayrıştırılıması için
 var bodyParser = require('body-parser');
 
 var db =require('./app_server/models/db');
 
-app.set('view engine','ejs');
-
-app.use(ejsLayouts);
-
-
+//uygulamamıza views set ettik
 app.set('views',path.join(__dirname,'./app_server/views'));
+
 
 
 // parse application/x-www-form-urlencoded
@@ -27,21 +42,25 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
-
-
-
-app.use('/public', express.static(path.join(__dirname, 'public')));
-
 require('./app_server/router/RouterManager')(app);
-
 var user= require('./app_server/models/user');
 
-// app.use(function(request,response,next){
-//     console.log('Gelen İstek', request.url);
-//     console.log('İstek Tarihi,', Date.now());
-//     next();
-// });
+
+//Statically serve files in these directories  -easyRTC
+app.use("/js", express.static(__dirname + '/node_modules/easyrtc/demos/js'));
+app.use("/images", express.static(__dirname + '/node_modules/easyrtc/demos/images'));
+app.use("/css", express.static(__dirname + '/node_modules/easyrtc/demos/css'));
 
 
 
-app.listen(8000)
+//Serve a static login page if not logged in already
+app.get('/konferans', function (req, res) {
+    console.log('Login attempt');
+    //true yazan kısma ileriki zamanlar oturum şartı geldiğinde düzenlicez.
+    if (true) {
+        res.sendfile(__dirname + '/node_modules/easyrtc/demos/demo_multiparty.html');
+    }
+    else {
+        res.sendfile(__dirname + '/public/login.html');
+    }
+});
